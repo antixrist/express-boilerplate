ea82c0d3ae518802da3c15bdb9fcea1876ad5e74
Лёха <kartashov.al.and@yandex.ru>
2016-11-07 15:02:44 +0400

    .

--- app/server.js
+++ app/server.js
@@ -263,4 +263,14 @@ app.use(function (err, req, res, next) {
   });
 });
 
+// todo
+// https://nodejs.org/docs/latest/api/process.html#process_event_uncaughtexception
+// process.on('uncaughtException', (err) => {
+//   fs.writeSync(1, `Caught exception: ${err}`);
+// });
+// process.on('unhandledRejection', (reason, p) => {
+//   console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+//   // application specific logging, throwing an error, or other logic here
+// });
+
 export default app;
